c8071bb0d6ccc37b4236b20fd20c52a514559c78
ajh322 <ajh322@gmail.com>
2022-01-02 16:52:47 +0900

    First Commit

---
+++ Assets/CharacterController.ts
@@ -0,0 +1,14 @@
+import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+import {SpawnInfo, ZepetoPlayers} from "ZEPETO.Character.Controller";
+
+export default class CharacterController extends ZepetoScriptBehaviour {
+
+    Start() {
+        ZepetoPlayers.instance.CreatePlayerWithZepetoId("ajh322", "ajh322", new SpawnInfo(), true);
+        ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(()=>{
+            let _player = ZepetoPlayers.instance.LocalPlayer;
+        })
+
+    }
+
+}
